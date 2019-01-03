68c9b58f04ffbf3b96f7ef6a0c21539d254fe37b
isham <isham4you@gmail.com>
2019-01-03 16:40:14 +0800

    Added comments

--- 4_DataTypes.ex
+++ 4_DataTypes.ex
@@ -1,9 +1,9 @@
 IO.puts 10
-IO.puts 0o13
-IO.puts 0b1111010
-IO.puts true
-IO.puts :atom
-IO.puts ["anne", "bell", "charlette"]
-IO.inspect {1,2,3}      #Inspect has been used coz puts throws error
+IO.puts 0o13            #Numberic but octal number
+IO.puts 0b1111010       #Numberic but binary number
+IO.puts true            #Super type of atom type
+IO.puts :atom           #Atom type
+IO.puts ["anne", "bell", "charlette"] #List
+IO.inspect {1,2,3}      #Tulip | Inspect has been used coz puts throws error
 IO.puts <<65,66,67>>    #Binary data to work with bits | This prints ABC
-IO.inspect <<65,66,67>> #This prints "ABC" (with double quotes)
+IO.inspect <<65,66,67>> #This prints "ABC" (with double quotess)
