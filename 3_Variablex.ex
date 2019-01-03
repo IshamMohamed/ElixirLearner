a87e698bce1dc90bf69110c43d5c3b1ae97f2045
isham <isham4you@gmail.com>
2019-01-03 15:38:36 +0800

    Double Quoted string

--- 3_Variablex.ex
+++ 3_Variablex.ex
@@ -4,3 +4,8 @@ IO.puts myIntegerVariable
 myStringVariable = 'Single Quoted Text'
 IO.puts myStringVariable
 
+myStringVariableDoubleQuote = "Double Quoted Text"
+IO.puts myStringVariableDoubleQuote
+
+stringVriableWithQuote = 'He said "I am done"'
+IO.puts stringVriableWithQuote
