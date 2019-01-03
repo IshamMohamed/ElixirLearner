edd7f6a5f377adf75d979b546b04f705bc872d0b
isham <isham4you@gmail.com>
2019-01-03 16:46:56 +0800

    Added byte type tp the list

--- 4_DataTypes.ex
+++ 4_DataTypes.ex
@@ -6,5 +6,5 @@ IO.puts :atom           #Atom type
 IO.puts ["anne", "bell", "charlette"] #List
 IO.inspect {1,2,3}      #Tulip | Inspect has been used coz puts throws error
 IO.puts <<65,66,67>>    #Binary data to work with bits | This prints ABC
-IO.inspect <<65,66,67>> #This prints "ABC" (with double quotess)
-IO.inspect [1, :name, "anne", false] #A single list can accomodate many types but cant use IO.puts
+IO.inspect <<65,64,67>> #This prints "ABC" (with double quotess)
+IO.inspect [1, :name, "anne", false, <<65,10,67>>] #A single list can accomodate many types but cant use IO.puts
