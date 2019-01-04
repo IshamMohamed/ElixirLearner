621547108cc048a717be36cc61194b06d8d03ff4
isham <isham4you@gmail.com>
2019-01-04 18:02:37 +0800

    Operators

---
+++ 5_Operators.ex
@@ -0,0 +1,13 @@
+var1 = true
+var2 = false
+IO.inspect var1 and var2
+IO.inspect var1 && var2
+
+var1 = 10
+var2 = 10
+IO.inspect var1 && var2
+#IO.inspect var1 and var2 <- as "and" expect the first value to be bool so,
+IO.inspect true and var1
+
+var1 = true
+IO.inspect !var1
