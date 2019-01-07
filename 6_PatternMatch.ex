25bcf9ea2e8665b954761caeb5063ef537670139
isham <isham4you@gmail.com>
2019-01-07 15:13:54 +0800

    Pattern Matching

---
+++ 6_PatternMatch.ex
@@ -0,0 +1,11 @@
+x = 12
+x = "Test"
+IO.puts x
+
+[var_1, _unused_var, var_2] = [{"First variable"}, 25, "Second variable" ]
+IO.inspect (var_1)
+IO.inspect(var_2)
+
+{height, width} = {13.56,6}
+IO.inspect height
+IO.inspect width
