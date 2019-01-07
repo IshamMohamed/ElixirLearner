6ec4e158db48d5fa6ed833e1f92cd120eb5bcc6d
isham <isham4you@gmail.com>
2019-01-07 15:17:29 +0800

    Pattern match with calculations

--- 6_PatternMatch.ex
+++ 6_PatternMatch.ex
@@ -9,3 +9,9 @@ IO.inspect(var_2)
 {height, width} = {13.56,6/2}
 IO.inspect height
 IO.inspect width
+#Pattern matching is matches the value in right side.
+
+#But if you want to do it in the other direction, match value in left, use "pin" operator
+
+^12 = b
+IO.inspect b
