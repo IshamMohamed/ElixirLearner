1cdbeedb623c50bb99c2346d4255b9d65db2c385
isham <isham4you@gmail.com>
2019-01-09 18:18:00 +0800

    Cond

--- 7_DecisionMaking.ex
+++ 7_DecisionMaking.ex
@@ -15,3 +15,13 @@ else
    IO.puts "Condition was satisfied!"
 end
 IO.puts "Outside the unless statement"
+
+#Cond condition
+#equivalent to If-elseif
+guess = 46
+cond do
+   guess == 10 -> IO.puts "You guessed 10!"
+guess == 46 -> IO.puts "You guessed 46!"
+ guess == 42 -> IO.puts "You guessed 42!"
+   true        -> IO.puts "I give up."
+end
