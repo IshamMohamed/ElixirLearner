9dbf835e92f09b463f32d70ca8ebe9dc8e7c8968
isham <isham4you@gmail.com>
2019-01-09 18:14:27 +0800

    Unless

--- 7_DecisionMaking.ex
+++ 7_DecisionMaking.ex
@@ -6,3 +6,11 @@ if a<5 do
 else
 IO.puts "Greater than 5"
 end
+
+#Unless condition
+a = false
+unless a === true do
+   IO.puts "Condition is not satisfied"
+   IO.puts "So this code block is executed"
+end
+IO.puts "Outside the unless statement"
