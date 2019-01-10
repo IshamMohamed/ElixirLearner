f8494440a1bb862ef07537eee68db2591192d206
isham <isham4you@gmail.com>
2019-01-10 11:12:09 +0800

    More string functions

--- 8_Strings.ex
+++ 8_Strings.ex
@@ -16,3 +16,14 @@ y = "Lumpur"
 z = x <> " " <> y
 IO.puts(z)
 #You cant do something like x = 4 and try to concatenate with string
+
+IO.puts(String.length("Hey! this is a long string"))
+IO.puts(String.reverse("mama"))
+IO.puts(Atom.to_string(:Isham))
+IO.puts(Atom.to_string(:Petaling) <> " " <> "Jaya")
+IO.puts("Value of PI is " <> Float.to_string(3.14))
+IO.puts(String.to_integer("345"))
+IO.inspect(Integer.parse("01")) #out put as  {1, ""}
+IO.inspect(Integer.parse("01.2")) # {1, ".2"}
+IO.puts(String.split("A.w.e.s.o.m.e","."))
+IO.puts(String.at("Name",1))
